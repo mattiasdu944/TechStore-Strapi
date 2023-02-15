e103c3c3ca803b63c09f8acda80884128fa4e063
Ultron <duartemattias3@gmail.com>
2023-02-15 23:18:25 +0100

    refactor

--- config/plugins.js
+++ config/plugins.js
@@ -10,7 +10,7 @@ module.exports = ({ env }) => ({
         providerOptions: {
           cloud_name: env('CLOUDINARY_NAME'),
           api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
+          api_secret: env('CLOUDINARY_SECRET'), 
         },
         actionOptions: {
           upload: {},
